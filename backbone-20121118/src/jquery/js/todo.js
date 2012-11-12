4da545aeb0b11724cc82f47672ca394d2b946026
Tangerine Cat <kaineer@gmail.com>
2012-11-12 12:58:08 +0600

    This way it does not work

--- backbone-20121118/src/jquery/js/todo.js
+++ backbone-20121118/src/jquery/js/todo.js
@@ -8,7 +8,7 @@ $(function() {
   };
 
   var addItem = function(note) {
-    items.push({id: genId(), note: note});
+    items.push({id: genId(), note: note, done: false});
   };
 
   var removeItem = function(id) {
@@ -22,7 +22,7 @@ $(function() {
   var renderItems = function() {
     return _(items).chain().map(function(item) {
       return "<div class='item'>" + 
-        "<div class='check'><input type='checkbox' id='" + item.id + "'></div>" +
+        "<div class='check'><input class='box' type='checkbox' id='" + item.id + "'></div>" +
         "<div class='note-text'>" + item.note + "</div>" +
         "</div>";
     }).inject(function(html, itemHtml) { return html + itemHtml; }, "").value();
@@ -37,4 +37,22 @@ $(function() {
   };
 
   renderContent();
+
+  var addTODO = function() {
+    addItem($("#todo").val());
+    renderContent();
+    $("#todo").val("");
+  };
+
+  $("#adding button").click(addTODO);
+  $("#todo").keydown(function(e) {
+    if(e.which == 13) { // Enter
+      addTODO();
+    }
+  });
+  
+  $(".check .box").click(function(e) {
+    alert(this.checked);
+  });
+  
 });
